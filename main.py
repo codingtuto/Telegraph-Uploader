bc13327b22dc251744bd782b1d2d9d2af5d6a874
Coding Team <87824092+codingtuto@users.noreply.github.com>
2023-05-25 11:22:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from telegraph import upload_file
 import pyrogram
 from pyrogram import filters, Client
+from graphlib import Config
 from pyrogram.types import (
     InlineQueryResultArticle, InputTextMessageContent,
     InlineKeyboardMarkup, InlineKeyboardButton,
